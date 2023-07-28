4f2f9af0319e653ca90a32f77bead47b0ca44e0a
Jorge Luiz <jorgeluiz006@hotmail.com>
2023-07-28 14:44:00 -0300

    FindById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -14,28 +14,27 @@ public class DB {
 	private static Connection conn = null;
 
 	public static Connection getConnection() {
-		if(conn == null) {
+		if (conn == null) {
 			try {
-			Properties props = loadProperties();
-			String url = props.getProperty("dburl");
-			conn = DriverManager.getConnection(url, props);
-		}
-			catch(SQLException e) {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 		return conn;
-	
+
 	}
-	
+
 	public static void closeConnection() {
-	if (conn != null) {
-		try {
-			conn.close();
-		}catch(SQLException e) {
-			throw new DbException(e.getMessage());
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
-	}
 	}
 
 	private static Properties loadProperties() {
@@ -47,7 +46,7 @@ public class DB {
 			throw new DbException(e.getMessage());
 		}
 	}
-	
+
 	public static void closeStatement(Statement st) {
 		if (st != null) {
 			try {
@@ -57,6 +56,7 @@ public class DB {
 			}
 		}
 	}
+
 	public static void closeResultSet(ResultSet rs) {
 		if (rs != null) {
 			try {
